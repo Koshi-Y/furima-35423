eff40167950188fb68988f61ceba71c7aed7f84e
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-06 17:33:19 +0900

    formにパスを記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :nickname,         presence: true
-with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} do
-  validates :last_name,        presence: true
-  validates :first_name,       presence: true
-end
-with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/} do
-  validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-end
-validates :birthday,         presence: true
+  validates :nickname,           presence: true
+  with_options presence: true,   format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+    validates :last_name,        presence: true
+    validates :first_name,       presence: true
+  end
+
+  with_options presence: true,   format: {with: /\A[ァ-ヶー－]+\z/} do
+    validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  end
+  validates :birthday,           presence: true
 
 end
