dd1941b091a10704132373de6105977b05d12d98
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-06 23:26:57 +0900

    Usreモデルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,           presence: true
-  with_options presence: true,   format: {with: /\A[ぁ-んァ-ン一-龥]/} do
-    validates :last_name,        presence: true
-    validates :first_name,       presence: true
+  with_options presence: true,   format: {with: /\A[ぁ-んァ-ン一-龥々]+\z/} do
+    validates :last_name
+    validates :first_name
   end
 
   with_options presence: true,   format: {with: /\A[ァ-ヶー－]+\z/} do
-    validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-    validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :last_name_kana
+    validates :first_name_kana
   end
+
   validates :birthday,           presence: true
+  validates :password,           format: {with: /\A[a-zA-Z0-9]+\z/}
 
 end
