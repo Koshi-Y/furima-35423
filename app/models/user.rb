7a5504a00dcfb927040ebc45feb29f37a50a6e09
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-10 14:54:51 +0900

    バリデーションとアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,6 @@ class User < ApplicationRecord
   validates :birthday,           presence: true
   validates :password,           format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
 
+  has_many :items
+
 end
