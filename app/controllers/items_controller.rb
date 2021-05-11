27c482dc23f5a8b87dd83d1a310f35445f00b061
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-11 14:53:44 +0900

    アクティブハッシュの導入とビューの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,24 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :price, :charges_id, :prefecture_id, :delivery_date_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :charges
+  belongs_to :condition
+  belongs_to :delivery_date
+  belongs_to :prefecture
+
+  has_one_attached :image
+  belongs_to :user
+
 
   validates :image,            presence: true
   validates :item_name,        presence: true
   validates :description,      presence: true
-  validates :category_id,      presence: true
-  validates :condition_id,     presence: true
+  validates :category_id,      presence: true, numericality: { other_than: 1 }
+  validates :condition_id,     presence: true, numericality: { other_than: 1 }
   validates :price,            presence: true, numericality: {in: 300..9999999}, format: { with: /\A[0-9]+\z/ }
   validates :charges_id,       presence: true
-  validates :prefecture_id,    presence: true
+  validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
   validates :delivery_date_id, presence: true
 
-  has_one_attached :image
-  belongs_to :user
+  
+
+  
+ 
 end
