48f6bb871f1fbebc33a6209fb384d57cea1dcc8b
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-12 11:04:44 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,19 +9,17 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-
-  validates :image,            presence: true
-  validates :item_name,        presence: true
-  validates :description,      presence: true
-  validates :category_id,      presence: true, numericality: { other_than: 1 }
-  validates :condition_id,     presence: true, numericality: { other_than: 1 }
-  validates :price,            presence: true, inclusion: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
-  validates :charges_id,       presence: true, numericality: { other_than: 1 }
-  validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
-  validates :delivery_date_id, presence: true, numericality: { other_than: 1 }
-
-  
-
-  
- 
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :description
+    validates :price,            inclusion: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :charges_id
+      validates :prefecture_id
+      validates :delivery_date_id
+    end
+  end
 end
