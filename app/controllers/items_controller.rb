59c99df9640a1eba49399ec8f0813867f3883f9e
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-12 16:40:52 +0900

    items_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
     @items = Item.all.order(created_at: :desc)
-    @charges = Charges.all
   end
 
   def new
