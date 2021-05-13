7a7af59bc58ac650d0359694df65f8d5ae155bd7
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-14 02:28:31 +0900

    商品を投稿したユーザー以外が商品の編集ページにアクセスするのを防ぐ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -41,5 +43,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :price, :charges_id,
                                  :prefecture_id, :delivery_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    unless (current_user == item.user) && user_signed_in? 
+      redirect_to action: :index
+    end
   end
 end
