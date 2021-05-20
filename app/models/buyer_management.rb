906309bd214c949f9bbafd458251b2a5398e228d
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-20 13:42:00 +0900

    クレジット購入機能の実装

--- app/models/buyer_management.rb
+++ app/models/buyer_management.rb
@@ -3,9 +3,10 @@ class BuyerManagement
   attr_accessor :token, :postal_code, :prefecture_id, :address1, :address2, :building_name, :phone_num, :user_id, :item_id  
 
   with_options presence: true do
-    validates :user_id, :item_id, :prefecture_id, :address1, :address2
+    validates :user_id, :item_id, :address1, :address2, :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_num, format: {with: /\A[0-9]+\z/, message: "is invalid"}
+    validates :phone_num, format: {with: /\A\d{10}$|^\d{11}\z/, message: "is invalid"}
+    validates :prefecture_id, numericality: { other_than: 1 }
   end
 
   def save
