7895ad886fb4001757f587101540f02d15f911f7
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-20 17:30:27 +0900

    出品者は購入機能に関与できない実装の修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,7 +42,7 @@ class OrdersController < ApplicationController
     end
 
     def move_to_index
-      if current_user == @item.user
+      if (current_user == @item.user) || (@item.management.present? )
         redirect_to root_path
       end
     end
