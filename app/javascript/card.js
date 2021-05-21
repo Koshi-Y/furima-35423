834bfa2728f764754adf9ae176cc49712622076b
Koshi-Y <tamazarashi_neo@icloud.com>
2021-05-21 14:45:53 +0900

    commit

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,6 +13,8 @@ const pay = () => {
       exp_year: `20${formData.get("buyer_management[exp_year]")}`,
     };
 
+    console.log(card)
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
@@ -26,7 +28,7 @@ const pay = () => {
       document.getElementById("card-exp-month").removeAttribute("name");
       document.getElementById("card-exp-year").removeAttribute("name");
 
-      document.getElementById("charge-form").submit();
+      // document.getElementById("charge-form").submit();
     });
   });
 };
